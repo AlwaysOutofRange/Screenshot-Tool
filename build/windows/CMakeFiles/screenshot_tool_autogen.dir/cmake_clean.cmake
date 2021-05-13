5337da05e15f738cdb9361d24c53846d9038188a
OutofRange-Nico <83180802+OutofRange-Nico@users.noreply.github.com>
2021-05-13 16:50:07 +0200

    Delete build directory

--- build/windows/CMakeFiles/screenshot_tool_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/screenshot_tool_autogen"
-  "screenshot_tool_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/screenshot_tool_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
